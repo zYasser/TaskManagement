e4b11b74bf8605ab64225b06f7efbba47ce53c27
yasserqamhiah <yaserqamhiah@gmail.com>
2024-03-10 16:38:04 +0300

    Set up project

---
+++ src/test/java/com/project/TaskManagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TaskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
